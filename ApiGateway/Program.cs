d8cd700b4354eece6a123a6816ccbef2603185d4
Anil Kaynar <anilkaynar@Anils-MacBook-Pro.local>
2024-12-14 13:35:15 +0300

    Api Gateway is now works development, development with docker-compose and production docker-compose.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -2,7 +2,21 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+var additionalEnvironmentInformation = Environment.GetEnvironmentVariable("ADDITIONAL_INFO");
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.{additionalEnvironmentInformation}.json", optional: true, reloadOnChange: true)
+
+        //ADDITIONAL_INFO
+        .AddEnvironmentVariables();
+});
+
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
